f619a4842558bbdcd2ffe22bb550c4ab8a545fee
Lee Rogers <edwinleerogers@gmail.com>
2018-11-27 11:04:29 -0800

    change git address to squirtle in package, remove test code from index, re-insert extra line in gitignore

--- src/index.js
+++ src/index.js
@@ -2,7 +2,3 @@ import App from './App';
 import style from './main.css';
 
 console.log('enter here!!');
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
